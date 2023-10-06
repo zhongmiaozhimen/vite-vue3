61a1b544fe47c4f097396e77bf8dbeeccc519936
tian mo <13307954270@189.cn>
2023-10-07 07:53:09 +0800

    update

--- assets/index-78dbcdfe.js
+++
@@ -1,1 +0,0 @@
-import{o as a,h as t,b as r,L as s,M as l,T as p,N as d}from"./index-bade218b.js";const i={__name:"index",props:{teleportToBody:{type:Boolean,default:!0}},setup(e){return(o,n)=>(a(),t(d,{to:"body",disabled:!e.teleportToBody},[r(p,s(l(o.$attrs)),null,16)],8,["disabled"]))}};export{i as _};
